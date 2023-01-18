56493fd5a136760eec612ca2d33f0c9af415fc4a
Marcelo Dellarmelina <marvin.dellarmelina@gmail.com>
2023-01-18 00:16:47 -0400

    inicio, blocos e comentarios

---
+++ fundamentos/bloco.js
@@ -0,0 +1,15 @@
+{console.log("Passo 01")
+console.log("Paso 02")
+console.log("Passo 03")
+
+}
+
+{
+console.log("Passo 04")
+console.log("Paso 05")
+}
+
+{
+    console.log("FIM")
+}
+
---
+++ fundamentos/inicio/senteca_ 1.js
@@ -0,0 +1,4 @@
+console.log("boa tarde")
+console.log("boa noite")
+console.log("bom dia")
+
